1d1f29726241887a94995d8bf5bc13b9bfe1644c
testautomation <kleszczynski@bluesoft.net.pl>
2018-05-21 12:22:34 +0200

    firstTest1 - test1

---
+++ qatestautomation/src/test/java/com/jsystem/testautomation/JUnitTest.java
@@ -0,0 +1,16 @@
+package com.jsystem.testautomation;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class JUnitTest {
+@Test
+    public void FirstTest(){
+    assertTrue( true==true);
+}
+
+
+
+
+}
